db94f1b48e8c68a257f2d7e288b4bfdd6b254cb7
Menezo <joaovitormenezesm12@gmail.com>
2022-12-17 02:53:54 -0300

    Solucao desafio 4

--- desafio_3/src/application/Program.java
+++ desafio_3/src/application/Program.java
@@ -5,28 +5,31 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
 		int n = sc.nextInt();
-		int k = sc.nextInt();
+		sc.nextLine();
 		
-		int[] arr = new int[n]; 
+		String[] linhas = new String[n]; 
 		
 		for (int i = 0; i < n; i++) {
-			arr[i] = sc.nextInt();
+			linhas[i] = sc.nextLine();
 		}
-		int contador = 0;
 		
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr.length; j++) {
-				if (arr[i] - arr[j] == k) {
-					contador++;
-				}
-			}
+		for (int i = 0; i < linhas.length; i++) {
+			String a = decifrar(linhas[i]);
+			System.out.println(a);
 		}
-		System.out.println(contador);
 		sc.close();
+	}
+	
+	public static String decifrar(String linha) {
+		int tamanho = (linha.length()) / 2;
+		String[] partes = {linha.substring(0, tamanho), linha.substring(tamanho)}; //Divide a linha ao meio e em seguida inverte cada parte
+		partes[0] = new StringBuilder(partes[0]).reverse().toString();
+		partes[1] = new StringBuilder(partes[1]).reverse().toString();
+		linha = partes[0].concat(partes[1]);
+		return linha;
 	}
 
 }
---
+++ desafio_4/src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	Scanner sc = new Scanner(System.in);
+	
+
+	
+	
+}
